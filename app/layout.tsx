07db30cc5dfabe1d2df00c06d13ee38bb4e983b9
Bandicoot Form <adityakbarp@gmail.com>
2024-12-31 18:28:17 +0000

    update layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -24,6 +24,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <Head>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
       <body className={`${poppins.className} bg-black text-white`}>
         <Navigation />
         <main>{children}</main>
