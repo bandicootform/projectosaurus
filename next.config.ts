e88c5351e126ba0d672ef7c4ba92200a05bf365f
Rex the Breathtaker <adityakbarp@gmail.com>
2025-01-01 22:28:04 +0700

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,16 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-        images: {
-        formats: ["image/webp"],
-        domains: ["https://bandicootform.vercel.app/work"]
-        
-   },
-  };
+  images: {
+    formats: ["image/webp"],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "bandicootform.vercel.app",
+        pathname: "/work/**",
+      },
+    ],
+  },
+};
 
 export default nextConfig;
