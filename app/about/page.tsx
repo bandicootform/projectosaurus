edf14da0213a9b9ab7ecdfc8c26c004e24f0f44b
Bandicoot Form <adityakbarp@gmail.com>
2025-01-01 04:51:31 +0000

    update about page and page.tsx

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -23,7 +23,7 @@ export default function About() {
           Bandicoot Form is a platform dedicated to explore the creativity, and meant to be seen as much as to be read. It serves as a space to transform basic ideas into visual expressions, while expanding the gorgeus garden of communication.
         </p>
         <p className="mb-4">
-          The focus is on the foundational steps, learning, creating, and having fun with it. Each work is a chance to dive deeper into the design process, share inspiration, and grow as a designer.
+          The focus is on the foundational steps, learning, creating, and having fun with it. Each work is a chance to dive deeper into the design process, share inspiration, and grow as whatever i want to be.
         </p> 
       </motion.div>
     </div>
