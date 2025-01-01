daca4c6f075e25dcc256f886ee26081f0603a392
Bandicoot Form <adityakbarp@gmail.com>
2025-01-01 02:42:59 +0000

    update navigation

--- app/components/Navigation.tsx
+++ app/components/Navigation.tsx
@@ -24,7 +24,7 @@ export default function Navigation() {
               <img
                 src="/images/logo.png" 
                 alt="Logo" 
-                className="h-16 w-auto" 
+                className="h-20 w-auto" 
                 />
           </Link>
           </div>
