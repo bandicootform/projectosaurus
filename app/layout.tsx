659d4c375f993841688a71fcbd99f9852cae4166
Bandicoot Form <adityakbarp@gmail.com>
2025-01-01 06:39:28 +0000

    update layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import Navigation from './components/Navigation'
 import Footer from './components/Footer'
 import { SpeedInsights } from "@vercel/speed-insights/next"
 import { Analytics } from "@vercel/analytics/react"
+import Head from 'next/head';
 
 const poppins = Poppins({
   weight: ['400', '600'],
@@ -25,6 +26,10 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" style={{ scrollBehavior: "smooth" }}>
+      <Head>
+        <link rel="stylesheet" 
+        href="https://fonts.cdnfonts.com/css/helvetica-neue-55" />
+        </Head>
       <body className={`${poppins.className} bg-black text-white`}>
         <Navigation />
         <main>{children}</main>
