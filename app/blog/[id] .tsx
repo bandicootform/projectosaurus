f5d48cc54372075c3642fe8f43a706cb13144511
Rex the Breathtaker <adityakbarp@gmail.com>
2025-01-03 07:53:05 +0700

    Update [id] .tsx

--- app/blog/[id] .tsx
+++ app/blog/[id] .tsx
@@ -1,16 +1,15 @@
 import { useEffect, useState } from 'react';
-import { client } from '../../sanity';
+import { client } from '../../sanity'; 
 import { useRouter } from 'next/router';
 
 const PostDetail = () => {
   const [post, setPost] = useState(null);
-  const { query } = useRouter();
+  const { query } = useRouter(); 
   const { id } = query;
 
   useEffect(() => {
     if (id) {
       const fetchPost = async () => {
-        
         const query = `*[_type == "post" && _id == $id][0]`;
         const data = await client.fetch(query, { id });
         setPost(data);
@@ -19,13 +18,18 @@ const PostDetail = () => {
     }
   }, [id]);
 
-  if (!post) return <div>Loading...</div>; 
+  if (!post) return <div>Loading...</div>;
 
   return (
     <div>
       <h1>{post.title}</h1>
       <p>{post.excerpt}</p>
-      <div>{post.content}</div> 
+      <div>{post.content}</div>
+      {post.coverImage && (
+        <img src={post.coverImage.asset.url} alt={post.title} />
+      )}
+      {post.date && <p>{new Date(post.date).toLocaleDateString()}</p>}
+      {post.author && <p>Written by: {post.author.name}</p>}
     </div>
   );
 };
